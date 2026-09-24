e2f3713ce2e931d53454769893700c949fd7cfe1
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-24 15:55:29 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+export const SYSTEM_CONFIG = {
+    appName: 'Nexus AI Engine',
+    version: '2.5.0-Enterprise',
+    manager: 'إدارة وتطوير أبو حازم العمري',
+    maxRequestLimit: 50,
+    windowMsTime: 15 * 60 * 1000
+};
